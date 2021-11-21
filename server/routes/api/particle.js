b3431d8cabd5ac6c4d0ebc1599cfd0bd6ca47f2d
JD <jdvillarroel@gmail.com>
2021-11-21 13:54:07 -0600

    Single conn to mongoDB and try catch added.

--- server/routes/api/particle.js
+++ server/routes/api/particle.js
@@ -6,10 +6,24 @@ require("dotenv").config();
 // MongoDB url string
 const mongoDBURL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@${process.env.MONGODB_CLUSTER}/${process.env.DB}?${process.env.OPTIONS}`;
 
+//Connects to mongoDB
+const client = async () => {
+  try {
+    const mongoClient = await mongodb.MongoClient.connect(mongoDBURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    return mongoClient;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // GET /api/particles
 router.get("/", async (req, res) => {
   // Get the particles collection
-  const particles = await loadParticles();
+  // const particles = await loadParticles();
+  const particles = client.db("home").collection("jOffice");
 
   // Fetch the last 8 hours of data
   const data = await particles
@@ -18,16 +32,14 @@ router.get("/", async (req, res) => {
     .limit(48)
     .toArray();
 
-  // Close connection to MongoDB
-  await particles.close();
-
   // Return the data as json
   res.send(data);
 });
 
 // POST /api/particles
 router.post("/", async (req, res) => {
-  const particles = await loadParticles();
+  // const particles = await loadParticles();
+  const particles = client.db("home").collection("jOffice");
 
   // Create a particle with the data sent from client
   let newData = {
@@ -41,20 +53,17 @@ router.post("/", async (req, res) => {
   // Insert the new particle into the collection
   const data = await particles.insertOne(newData);
 
-  // Close database connection
-  await particles.close();
-
   // Send response to the client
   res.status(201).send();
 });
 
 // Load the particles collection
-const loadParticles = async () => {
-  const client = await mongodb.MongoClient.connect(mongoDBURL, {
-    useNewUrlParser: true,
-  });
+// const loadParticles = async () => {
+//   const client = await mongodb.MongoClient.connect(mongoDBURL, {
+//     useNewUrlParser: true,
+//   });
 
-  return client.db("home").collection("jOffice");
-};
+//   return client.db("home").collection("jOffice");
+// };
 
 module.exports = router;
